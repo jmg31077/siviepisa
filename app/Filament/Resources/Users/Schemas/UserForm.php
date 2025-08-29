7886f5131ca0de1f0b2c977b1799c12c3fe3f527
jmg31077 <jhesua2009@gmail.com>
2025-08-28 22:54:31 -0400

    mejorar codigo

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -2,8 +2,20 @@
 
 namespace App\Filament\Resources\Users\Schemas;
 
-use Filament\Forms\Components\DateTimePicker;
+// use Filament\Forms\Components\DateTimePicker;
+
+use App\Models\State;
+use App\Models\User;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
+use Filament\Forms\Components\Select;
+// use Filament\Notifications\Collection;
+// use Filament\Forms\Get;
+// use Filament\Forms\Form;
+// use Filament\Table;
+// use Filament\Resources\Resource;
+use Illuminate\Support\Collection;
+
 use Filament\Schemas\Schema;
 
 class UserForm
@@ -12,16 +24,49 @@ class UserForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->required(),
-                TextInput::make('email')
-                    ->label('Email address')
-                    ->email()
-                    ->required(),
+                Section::make('Personal info')
+                ->columns(3)
+                    ->schema([
+
+                    TextInput::make('name')
+                       ->required(),
+                    TextInput::make('email')
+                       ->label('Email address')
+                       ->email()
+                       ->required(),
                 // DateTimePicker::make('email_verified_at'),
-                TextInput::make('password')
-                    ->password()
-                    ->required(),
-            ]);
+                    TextInput::make('password')
+                        ->password()
+                        ->required(),
+                    ]),
+                Section::make('Address info')
+                ->columns(3)
+                    ->schema([
+
+                    Select::make('country_id')
+                        ->relationship(name: 'country',titleAttribute: 'name')
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->required(),
+                    Select::make('state_id')
+                        ->options(fn (Get $get): Collection => State::query()
+                        ->where('country_id', $get('country_id'))
+                        ->pluck('name', 'id'))
+
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->required(),
+                    Select::make('city_id')
+
+                        ->relationship(name: 'city', titleAttribute: 'name')
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->required(),
+
+            ])
+                    ]);
     }
 }
